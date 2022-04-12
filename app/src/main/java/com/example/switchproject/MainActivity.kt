0d44401e5b930f226037600fa839375b6f2ad31d
RPh2coder67 <jpdcands211@gmail.com>
2022-04-12 12:03:36 -0400

    Initial commit

---
+++ app/src/main/java/com/example/switchproject/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.switchproject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Switch
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.switchproject.ui.theme.SwitchProjectTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SwitchProjectTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    CustomSwitch()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CustomSwitch() {
+    val checked = remember { mutableStateOf(true) }
+
+    Column {
+        Switch(
+            checked = checked.value,
+            onCheckedChange = { checked.value = it }
+        )
+        if (checked.value) {
+            Text("Switch is On")
+        } else {
+            Text("Switch is Off")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SwitchProjectTheme {
+        CustomSwitch()
+    }
+}
